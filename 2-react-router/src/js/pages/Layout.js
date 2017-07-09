3848de7b2c1ba53fede0d7de2cebc861361d4a61
farmok-win <farmok@gmail.com>
2017-07-09 07:41:00 -0400

    Added additoinal componenets to start setting up the single page app.

---
+++ 2-react-router/src/js/pages/Layout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default class Layout extends React.Component {
+  navigate() {
+    this.props.history.replaceState(null, "/");
+  }
+  render() {
+    const { history } = this.props;
+    console.log(history.isActive("archives"));
+    return (
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="archives" activeClassName="test"><button class="btn btn-danger">archives</button></Link>
+        <Link to="settings"><button class="btn btn-success">settings</button></Link>
+        <button onClick={this.navigate.bind(this)}>featured</button>
+      </div>
+    )
+  }
+}
